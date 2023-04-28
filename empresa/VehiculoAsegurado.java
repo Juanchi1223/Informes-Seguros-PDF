bedfd3f42856ede455e22f814c716e0d20b1ec74
unknown <juanchicanestrari@gmail.com>
2023-04-28 16:46:09 -0300

    Refactor, nuevo modelo de clases

---
+++ empresa/VehiculoAsegurado.java
@@ -0,0 +1,128 @@
+package empresa;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class VehiculoAsegurado implements Serializable {
+		protected String daños;
+		protected String codigo;
+		protected String tipo;
+		protected String propietario;
+		protected String marca;
+		protected String modelo;
+		protected int año;
+		protected String dominio;
+		protected int nroChasis;
+		protected int nroMotor;
+		protected String denuncia;
+		protected String documentacion;
+		protected Fotos docFotos;
+		protected String justificarDocs;
+		protected Fotos rodadoFotos;
+		protected Fotos cotizacion;
+		protected ArrayList<Tripulantes> listaTripulantes = new ArrayList<Tripulantes>();
+		
+		public String getDaños() {
+			return daños;
+		}
+		public String getCodigo() {
+			return codigo;
+		}
+		public String getTipo() {
+			return tipo;
+		}
+		public String getPropietario() {
+			return propietario;
+		}
+		public String getMarca() {
+			return marca;
+		}
+		public String getModelo() {
+			return modelo;
+		}
+		public int getAño() {
+			return año;
+		}
+		public String getDominio() {
+			return dominio;
+		}
+		public int getNroChasis() {
+			return nroChasis;
+		}
+		public int getNroMotor() {
+			return nroMotor;
+		}
+		public String getDenuncia() {
+			return denuncia;
+		}
+		public String getDocumentacion() {
+			return documentacion;
+		}
+		public Fotos getDocFotos() {
+			return docFotos;
+		}
+		public String getJustificarDocs() {
+			return justificarDocs;
+		}
+		public Fotos getRodadoFotos() {
+			return rodadoFotos;
+		}
+		public Fotos getCotizacion() {
+			return cotizacion;
+		}
+		public ArrayList<Tripulantes> getListaTripulantes() {
+			return listaTripulantes;
+		}
+		
+		public void setDaños(String daños) {
+			this.daños = daños;
+		}
+		public void setCodigo(String codigo) {
+			this.codigo = codigo;
+		}
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+		public void setPropietario(String propietario) {
+			this.propietario = propietario;
+		}
+		public void setMarca(String marca) {
+			this.marca = marca;
+		}
+		public void setModelo(String modelo) {
+			this.modelo = modelo;
+		}
+		public void setAño(int año) {
+			this.año = año;
+		}
+		public void setDominio(String dominio) {
+			this.dominio = dominio;
+		}
+		public void setNroChasis(int nroChasis) {
+			this.nroChasis = nroChasis;
+		}
+		public void setNroMotor(int nroMotor) {
+			this.nroMotor = nroMotor;
+		}
+		public void setDenuncia(String denuncia) {
+			this.denuncia = denuncia;
+		}
+		public void setDocumentacion(String documentacion) {
+			this.documentacion = documentacion;
+		}
+		public void setDocFotos(Fotos docFotos) {
+			this.docFotos = docFotos;
+		}
+		public void setJustificarDocs(String justificarDocs) {
+			this.justificarDocs = justificarDocs;
+		}
+		public void setRodadoFotos(Fotos rodadoFotos) {
+			this.rodadoFotos = rodadoFotos;
+		}
+		public void setCotizacion(Fotos cotizacion) {
+			this.cotizacion = cotizacion;
+		}
+		public void setListaTripulantes(ArrayList<Tripulantes> listaTripulantes) {
+			this.listaTripulantes = listaTripulantes;
+		}
+}
--- empresa/VehiculosLesionados.java
+++ empresa/VehiculosLesionados.java
@@ -1,5 +1,37 @@
 package empresa;
 
-public class VehiculosLesionados {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+public class VehiculosLesionados implements Serializable {
+	private ArrayList<Vehiculos> listaVehiculosAsegurados;
+	private ArrayList<Vehiculos> listaVehiculosTerceros;
+	private int codigo = 0;
+
+	
+	
+	public void agregarVehiculoAs(Vehiculos rodado) {
+		this.codigo++;
+		rodado.setCodigo(Integer.toString(codigo));
+		this.listaVehiculos.add(rodado);
+	}
+	public void borrarVehiculo(int posicion) {
+		this.listaVehiculos.remove(posicion);
+	}
+	
+	public ArrayList<Vehiculos> listaVehiculosAsegurados() {
+		return listaVehiculosAsegurados;
+	}
+
+	public void listaVehiculosAsegurados(ArrayList<Vehiculos> listaVehiculos) {
+		this.listaVehiculosAsegurados = listaVehiculos;
+	}
+
+	public ArrayList<Vehiculos> getListaVehiculosTerceros() {
+		return listaVehiculosTerceros;
+	}
+
+	public void setListaVehiculosTerceros(ArrayList<Vehiculos> listaVehiculosTerceros) {
+		this.listaVehiculosTerceros = listaVehiculosTerceros;
+	}
 }
